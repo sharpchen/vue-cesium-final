e49106c042e5ea54c715b9ba23f7465874cc5c8a
sharpchen <rui.chen.sharp@gmail.com>
2024-02-14 14:51:00 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-  import { VcViewer } from 'vue-cesium';
+  import { VcPrimitiveTileset, VcViewer } from 'vue-cesium';
 </script>
 
 <template>
-  <VcViewer style="width: 100%; height: 100%; position: fixed; top: 0; left: 0" />
+  <VcViewer style="width: 100%; height: 100%; position: fixed; top: 0; left: 0">
+    <VcPrimitiveTileset
+      url="https://zouyaoji.top/vue-cesium/SampleData/Cesium3DTiles/Tilesets/dayanta/tileset.json">
+    </VcPrimitiveTileset>
+  </VcViewer>
 </template>
 
 <style scoped>
